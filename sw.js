6b271edd5a1709184747dee5e80abdd19125bdf5
tharindunavod98-lab <tharindunavod98@gmail.com>
2026-04-03 04:49:13 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE = 'stylebiz-mv-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/index.html']))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
